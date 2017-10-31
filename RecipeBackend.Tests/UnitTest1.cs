8de6e250d67ac75a1ed58ca6a66fa6325d6d1aa2
Joshua Novick <jnovick@ufl.edu>
2017-10-30 22:26:51 -0400

    Add Travis
    * A test project was created but no tests have been written

---
+++ RecipeBackend.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RecipeBackend.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //At least one test class and one test is required to pass travis. A real test class will be added soon.
+        }
+    }
+}
